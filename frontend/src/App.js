1646d3b558608b64d5d55a8bc3388f1568cfb9f9
Marius Bogdanel <marius.bogdanel@gmail.com>
2021-04-09 17:15:34 +0100

    started the cartScreen, reducers and action for add to cart functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 import InProgress from './components/InProgress'
 
 const App = () => {
@@ -16,10 +17,10 @@ const App = () => {
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
-
     </Router>
   )
 }
