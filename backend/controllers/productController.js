f931b28dac0bf64bbf60b29920831e028d9a80c6
Marius Bogdanel <marius.bogdanel@gmail.com>
2024-02-10 11:39:18 +0000

    update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js'
 // @route GET /api/products
 // @acces Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 10
+  const pageSize = process.env.PAGINATION_LIMIT
   const page = Number(req.query.pageNumber) || 1
 
   const keyword = req.query.keyword
